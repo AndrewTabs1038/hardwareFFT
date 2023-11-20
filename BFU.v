4db5e07f9e64dbe44f2e1792871275aab48ae5a0
AndrewTabs1038 <135442448+AndrewTabs1038@users.noreply.github.com>
2023-11-20 11:24:07 -0600

    Update BFU.v

--- BFU.v
+++ BFU.v
@@ -6,15 +6,10 @@ input signed [DATA_WIDTH-1:0] A_in_r, B_in_r, A_in_i, B_in_i, twiddleF_r, twiddl
 output reg signed [DATA_WIDTH-1:0] A_out_r, B_out_r, A_out_i, B_out_i
 );
 
-
-
 reg signed [(DATA_WIDTH*2)- 1:0] imagP_one, imagP_two;
 reg signed [(DATA_WIDTH*2)- 1:0] realP_one, realP_two;
 
 reg signed [DATA_WIDTH-1:0] imagS, realS;
-
-//reg signed [DATA_WIDTH - 1:0] temp_A_r, temp_A_i, temp_B_r, temp_B_i;
-
 
 wire signed [DATA_WIDTH-1:0] A_dout_r;
 wire signed [DATA_WIDTH-1:0] A_dout_i;
@@ -56,8 +51,7 @@ Ai //imaginary value
 //The pipeline is as follows:
 //Calculate products within complex multicplication ->
 //Perform a turnincated within the complex mulplicaiton for correct magnitude ->
-//addition with the now aligned A values ->
-//set outputs
+//addition with the now aligned A values 
 
 always @(posedge clk) begin
 	
@@ -65,28 +59,19 @@ always @(posedge clk) begin
 	//Cycle 1 -> Calculates the products 
 	imagP_one <= (B_in_r * twiddleF_i);
 	imagP_two <= (B_in_i * twiddleF_r);
-   realP_one <= (B_in_r * twiddleF_r);
+        realP_one <= (B_in_r * twiddleF_r);
 	realP_two <= (B_in_i * twiddleF_i);
 	
 	//Cycle 2 -> Turnicated addition
 	imagS <= imagP_one[(DATA_WIDTH*2) - 2:DATA_WIDTH-1] + imagP_two[(DATA_WIDTH*2) - 2:DATA_WIDTH-1];
 	realS <= realP_one[(DATA_WIDTH*2) - 2:DATA_WIDTH-1]- realP_two[(DATA_WIDTH*2) - 2:DATA_WIDTH-1] ;
-
+	
+	//Cycle 3 -> Addition
 	A_out_r <= A_dout_r + realS;
 	A_out_i <= A_dout_i + imagS;
 	B_out_r <= A_dout_r - realS;
 	B_out_i <= A_dout_i - imagS;
-//	//Cycle 3 -> Addition
-//	temp_A_r <= A_dout_r + realS;
-//	temp_A_i <= A_dout_i + imagS;
-//	temp_B_r <= A_dout_r - realS;
-//	temp_B_i <= A_dout_i - imagS;
-//	
-//	//Cycle 4 -> Set outputs 
-//	A_out_r <= temp_A_r;
-//	A_out_i <= temp_A_i;
-//	B_out_r <= temp_B_r;
-//	B_out_i <= temp_B_i;
+
 
 
 end
